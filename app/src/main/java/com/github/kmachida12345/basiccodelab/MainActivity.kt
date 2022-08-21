e5d0136bef4522b7b93f79783b756834e6a9efad
kohei.machida <kohei.machida@a10lab.com>
2022-08-21 09:45:16 +0900

    Compose の状態

--- app/src/main/java/com/github/kmachida12345/basiccodelab/MainActivity.kt
+++ app/src/main/java/com/github/kmachida12345/basiccodelab/MainActivity.kt
@@ -8,6 +8,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,14 +33,18 @@ fun Greeting(name: String) {
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
+        val expanded = remember { mutableStateOf(false) }
+        val extraPadding = if (expanded.value) 48.dp else 0.dp
+
         Row(modifier = Modifier.padding(24.dp)) {
             Column(modifier = Modifier
-                .weight(1f)) {
+                .weight(1f)
+                .padding(bottom = extraPadding)) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
-            OutlinedButton(onClick = { /*TODO*/ }) {
-                Text(text = "Show more")
+            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(text = if (expanded.value) "Show less" else "Show more")
             }
         }
     }
