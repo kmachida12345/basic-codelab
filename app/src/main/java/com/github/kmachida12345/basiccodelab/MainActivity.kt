56ffeddd0389719cac350c0f7cf3bfe2a1550856
kohei.machida <kohei.machida@a10lab.com>
2022-08-21 09:35:24 +0900

    wip

--- app/src/main/java/com/github/kmachida12345/basiccodelab/MainActivity.kt
+++ app/src/main/java/com/github/kmachida12345/basiccodelab/MainActivity.kt
@@ -5,11 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,9 +32,12 @@ fun Greeting(name: String) {
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(modifier = Modifier
-                .fillMaxWidth()) {
+                .weight(1f)) {
                 Text(text = "Hello, ")
                 Text(text = name)
+            }
+            OutlinedButton(onClick = { /*TODO*/ }) {
+                Text(text = "Show more")
             }
         }
     }
